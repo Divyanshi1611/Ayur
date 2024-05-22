abfcc180764d192d025b2830344e8098c6b4a842
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2024-05-23 01:08:41 +0530

    modified

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def main():
+    st.set_page_config(
+        page_title="Your own health assistant",
+        page_icon="👩‍⚕️💊"
+           
+    )
+    st.header("AYUR--Health assistant 👩‍⚕️")
+if __name__=="__main__":
+    main()
